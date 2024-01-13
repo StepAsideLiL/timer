85e1fe2e2e5a04860ecdcbde1d0274575e2d7299
Rifat Khan <rifat6633@gmail.com>
2024-01-13 13:20:46 +0600

    delete

--- app/[time]/page.tsx
+++
@@ -1,5 +0,0 @@
-const TimeIdPage = ({ params }: { params: { time: string } }) => {
-  return <main className="container">TimeIdPage {params.time}</main>;
-};
-
-export default TimeIdPage;
--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-const HomePage = () => {
-  return <div className="flex items-center justify-center">Hello, World!</div>;
-};
-
-export default HomePage;
