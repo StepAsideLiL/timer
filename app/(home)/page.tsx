029c9ab4a03a5e45aee9f1d75430a12f9426db56
Rifat Khan <rifat6633@gmail.com>
2024-01-13 13:21:17 +0600

    create home page and add SetTimer

---
+++ app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import SetTimer from "./_parts/set-timer";
+
+const HomePage = () => {
+  return (
+    <main className="container flex items-center justify-center">
+      <section>
+        <SetTimer />
+      </section>
+    </main>
+  );
+};
+
+export default HomePage;
